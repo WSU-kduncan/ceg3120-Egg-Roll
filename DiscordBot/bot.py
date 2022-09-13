99b75bfb427190d823e44f84ca8fbade3f3db557
Justin <89227698+Egg-Roll@users.noreply.github.com>
2022-09-13 11:53:40 -0400

    Update bot.py

--- DiscordBot/bot.py
+++ DiscordBot/bot.py
@@ -39,15 +39,15 @@ async def on_message(message):
         'Now, I know what you are gonna say: "Technoblade, you monster! You really killed millions of your own kind just to make a sword?". And it- look, look, look, come on, you gotta understand. There was nothing else I could do, I was mildly inconvenienced!',
 	'Most opportunities are created by luck. It takes skill to grasp those opportunities and turn it into success.',
     ]
-	if message.content == 'technoblade!':
-	if message.content.startswith('$technoblade'):
-        	response = random.choice(technoblade_quotes)
-        	await message.channel.send(response)
+    if message.content == 'technoblade!':
+    if message.content.startswith('$technoblade'):
+        response = random.choice(technoblade_quotes)
+        await message.channel.send(response)
 
 @client.event
 async def on_message(message):
-	if message.content == 'pig!':
-	if message.content.startswith('pig'):
-		await message.channel.send(file=discord.File(random.choice('pig1.JPG', 'pig22.JPG', 'pig3.JPG')))
+    if message.content == 'pig!':
+    if message.content.startswith('pig'):
+	await message.channel.send(file=discord.File(random.choice('pig1.JPG', 'pig22.JPG', 'pig3.JPG')))
 
 client.run(TOKEN)
